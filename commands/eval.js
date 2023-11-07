70fdd0fd3f5c320ee96c387265e6f6ae57981a64
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-07 15:08:36 -0500

    update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -3,40 +3,23 @@ const { format } = require('util');
 
 const assistant = '🚩 Simple Base Wa Bot';
 
-const reply = async (client, from, v, text) => {
-    const msg = generateWAMessageFromContent(from, {
-        extendedTextMessage: {
-            text,
-            contextInfo: {
-                externalAdReply: {
-                    title: assistant,
-                    showAdAttribution: true,
-                    thumbnailUrl: 'https://telegra.ph/file/a88de6973f18046e409a9.jpg'
-                }
-            }
-        }
-    }, { quoted: v });
-    await client.sendMessage(from, msg.message, {});
-};
+const msg = (jid, content, options) =>  client.sendMessage(jid, content, options);
+const evaluate = text => takuMsg(from, { text, linkPreview: {} }, { quoted: v })
 
-const evalCommand = async (client, from, v, body) => {
-    if (body.startsWith('>')) {
-        try {
-            let value = await eval(`(async() => { ${body.slice(1)} })()`);
-            await reply(client, from, v, format(value));
-        } catch (e) {
-            await reply(client, from, v, e.toString());
-        }
+const eval = async (client, from, v, body) => {
+    // Verifica si el remitente es el usuario autorizado
+    if (v.key.participant !== AUTHORIZED_USER) {
+        return;
     }
-    
-    if (body.startsWith('<')) {
+
+    if (body.startsWith('=>')) {
         try {
-            let value = await eval(`(async() => { return ${body.slice(1)} })()`);
-            await reply(client, from, v, format(value));
+            let value = await eval(`(async() => { return ${body.slice(2)} })()`);
+            await evaluate(client, from, v, format(value));
         } catch(e) {
-            await reply(client, from, v, e.toString());
+            await evaluate(client, from, v, e.toString());
         }
     }
 };
 
-module.exports = { reply, evalCommand };
+module.exports = { msg, evaluate, eval };
