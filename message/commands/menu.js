328bce762cc33b7505236ed660d1e4c765dba3ac
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-16 11:04:24 -0500

    update menu.js

--- message/commands/menu.js
+++ message/commands/menu.js
@@ -7,24 +7,37 @@ module.exports = {
 
     async execute(sock, m) {
         const user = m.sender.split('@')[0];
-        const prefixList = global.prefix.map(p => `[ ${p} ]`).join(' / ');
+        const prefixList = global.prefix.map(p => `[ ${p} ]`).join(' ');
 
         const uptimeSeconds = Math.floor(process.uptime());
         const days = Math.floor(uptimeSeconds / (24 * 60 * 60));
         const hours = Math.floor((uptimeSeconds % (24 * 60 * 60)) / (60 * 60));
         const minutes = Math.floor((uptimeSeconds % (60 * 60)) / 60);
+        const seconds = uptimeSeconds % 60;
 
-        await v.reply(`    *Hola @${user} 🍥*
+        await v.reply(`*Hola @${user} 🍥*
 ᳃ "Es momento de levantarse y dar pasos largos para lograr nuestros objetivos"
 
-  *Prefijo:* ${prefixList} 
-  *Modo:* Público
-  *Actividad:* ${days > 0 ? `${days}d ` : ''}${hours}h ${minutes}m
+*Prefijo:* ${prefixList} 
+*Modo:* Público
+*Actividad:* ${days > 0 ? `${days}d ` : ''}${hours}h ${minutes}m ${seconds}s
 
 Para obtener información de algún comando usa "Help <command>"
 
 Comandos disponibles:
 - Test
-- Ping`, { quoted: m });
+- Ping`, {
+            contextInfo: {
+                mentionedJid: [m.sender],
+                externalAdReply: {
+                    title: `Kaori 🍥`,
+                    body: `por @alexito`,
+                    showAdAttribution: true,
+                    renderLargerThumbnail: false,
+                    mediaType: 1,
+                    thumbnailUrl: 'https://telegra.ph/file/ae78c6675b0f413a5c635.jpg'
+                }
+            }
+        });
     }
 };
