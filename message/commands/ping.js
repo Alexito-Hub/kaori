b58b7b186d94ff30e7f6d8af3333aabd194ed76d
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-15 20:29:49 -0500

    up

--- message/commands/ping.js
+++ message/commands/ping.js
@@ -4,6 +4,13 @@ module.exports = {
     aliases: ['ping', 'ms'],
     
     async execute(sock, m, args) {
+        const uptime = process.uptime();
+        
+        const days = Math.floor(uptime / (24 * 60 * 60));
+        const hours = Math.floor((uptime % (24 * 60 * 60)) / (60 * 60));
+        const minutes = Math.floor((uptime % (60 * 60)) / 60);
+        const seconds = Math.floor(uptime % 60);
+        
         function roundTime(time) {
             return Math.round(time);
         }
