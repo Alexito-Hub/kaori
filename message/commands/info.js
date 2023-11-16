76f4443a7c8d16a5aa2e532f48618df235aeac75
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-16 09:25:00 -0500

    up

--- message/commands/info.js
+++ message/commands/info.js
@@ -3,32 +3,9 @@
 module.exports = {
   name: 'info',
   description: 'Obtener información sobre un comando',
-  aliases: ['commandinfo'],
+  aliases: ['commandinfo', 'info', 'información'],
 
   async execute(sock, m, args) {
-    const query = args[0];
-
-    if (!query) {
-      v.reply('Por favor, proporciona el nombre o alias del comando para obtener información.');
-      return;
-    }
-
-    const commandInfo = commands.find(cmd => cmd.name.toLowerCase() === query.toLowerCase() || (cmd.aliases || []).map(alias => alias.toLowerCase()).includes(query.toLowerCase()));
-
-    if (!commandInfo) {
-      v.reply(`El comando o alias '${query}' no existe.`);
-      return;
-    }
-
-    const isEnabled = commandInfo.isEnabled !== undefined ? commandInfo.isEnabled : true;
-
-    const infoMessage = `
-Nombre: ${commandInfo.name}
-Descripción: ${commandInfo.description || 'No hay descripción disponible.'}
-Alias: ${commandInfo.aliases ? commandInfo.aliases.join(', ') : 'No hay alias disponibles.'}
-Estado de comando: ${isEnabled ? 'Habilitado' : 'Deshabilitado'}
-    `;
-
-    v.reply(infoMessage);
+    v.reply(`Error de codigo`)
   },
 };
--- message/commands/ping.js
+++ message/commands/ping.js
@@ -6,33 +6,11 @@ module.exports = {
     aliases: ['ping', 'ms'],
     
     async execute(sock, m, args) {
-        const uptime = process.uptime();
+        const startTime = Date.now();
         
-        const days = Math.floor(uptime / (24 * 60 * 60));
-        const hours = Math.floor((uptime % (24 * 60 * 60)) / (60 * 60));
-        const minutes = Math.floor((uptime % (60 * 60)) / 60);
-        const seconds = Math.floor(uptime % 60);
+        const endTime = Date.now();
+        const responseTime = endTime - startTime;
         
-        function roundTime(time) {
-            return Math.round(time);
-        }
-        
-        const responseMs = Date.now();
-        const responseTime = roundTime(responseMs - m.messageTimestamp * 1000);
-        const formattedResponseTime = (responseTime / 1000).toFixed(3);
-        
-        v.reply({
-            text: `*Tiempo de respuesta:* ${formattedResponseTime} ms`,
-            contextInfo: {
-                externalAdReply: {
-                    title: `Kaori`,
-                    body: `${days} días ${hours} horas ${minutes} minutos ${seconds} segundos`,
-                    showAdAttribution: true,
-                    renderLargerThumbnail: false, 
-                    mediaType: 1, 
-                    thumbnailUrl: 'https://telegra.ph/file/ae78c6675b0f413a5c635.jpg'
-                }
-            }
-        }, { quoted: m });
+        v.reply(`Tiempo de respuesta: ${responseTime} ms`, { quoted: m });
     }
 };
