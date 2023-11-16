a14eebc74ad06bf197c2d36dbc580dc0e57ff9ab
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-16 18:00:01 -0500

    update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -64,7 +64,7 @@ module.exports = async(sock, m, store) => {
         }
         
         const messageKa = (jid, content, options) =>  sock.sendMessage(jid, content, options);
-        const kaoriMs = text => messageKa(from, { text, linkPreview: {} }, { quoted: v })
+        const kaoriMs = text => messageKa(from, { text, linkPreview: {} }, { quoted: m })
      
         
 		switch (command) {
