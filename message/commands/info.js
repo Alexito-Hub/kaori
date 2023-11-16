307cb091a0a5540c0de3703e16b23a56f2bffc10
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-15 19:39:12 -0500

    update info.js

--- message/commands/info.js
+++ message/commands/info.js
@@ -2,19 +2,33 @@
 
 module.exports = {
   name: 'info',
-  aliases: [],
-  description: 'Obtiene información sobre un comando específico',
+  description: 'Obtener información sobre un comando',
+  aliases: ['info', 'información'],
 
   async execute(sock, m, args) {
-    const requestedCommand = args[0];
-    const commandInfo = getCommandInfo(requestedCommand.toLowerCase());
+    const commandName = args[0];
 
-    if (commandInfo) {
-      const status = commandInfo.disabled ? 'deshabilitado' : 'habilitado';
-      const infoMessage = `Nombre: ${commandInfo.name}\nDescripción: ${commandInfo.description}\nAlias: ${commandInfo.aliases.join(', ')}\nEstado del comando: ${status}`;
-      v.reply(m.chat, infoMessage);
-    } else {
-      v.reply(m.chat, `El comando ${requestedCommand} no existe.`);
+    if (!commandName) {
+      v.reply('Por favor, proporciona el nombre o alias del comando para obtener información.');
+      return;
     }
+
+    const commandInfo = getCommandInfo(commandName.toLowerCase());
+
+    if (!commandInfo) {
+      v.reply(m.chat, `El comando o alias '${commandName}' no existe.`);
+      return;
+    }
+
+    const isEnabled = commandInfo.isEnabled !== undefined ? commandInfo.isEnabled : true;
+
+    const infoMessage = `
+Nombre: ${commandInfo.name}
+Descripción: ${commandInfo.description || 'No hay descripción disponible.'}
+Alias: ${commandInfo.aliases ? commandInfo.aliases.join(', ') : 'No hay alias disponibles.'}
+Estado de comando: ${isEnabled ? 'Habilitado' : 'Deshabilitado'}
+    `;
+
+    v.reply(m.chat, infoMessage);
   },
 };
