c476f44b45eac73c897c4a4115c20f8d7b31b3c4
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-17 21:03:29 -0500

    update version.js

---
+++ message/commands/version.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+module.exports = {
+    name: 'version',
+    description: 'Muestra la última versión del repositorio',
+    aliases: ['version', 'v'],
+
+    async execute(sock, m) {
+        try {
+            const repoOwner = 'Alexito-Hub'
+            const repoName = 'Kaori'
+
+            const response = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/releases/latest`);
+
+            const latestVersion = response.data.tag_name;
+            const releaseNotes = response.data.body;
+
+            await sock.sendMessage(m.chat, `Nueva versión: ${latestVersion}
+Notas de la versión:
+${releaseNotes}`, { quoted: m });
+        } catch (error) {
+            console.error(error);
+            await sock.sendMessage(m.chat, 'Parece que hubo un error', { quoted: m });
+        }
+    }
+}
