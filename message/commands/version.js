32b38579fb95d18319da6be262efbe045b76f959
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-18 00:22:15 -0500

    update version.js

--- message/commands/version.js
+++ message/commands/version.js
@@ -1,40 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const versionFilePath = path.join(__dirname, '../version.json');
+const versionFile = path.join(__dirname, '../version.json');
 
 module.exports = {
     name: 'version',
     description: 'Muestra la versión actual o la lista de versiones',
-    aliases: ['version', 'versions'],
+    aliases: ['version', 'v'],
 
     async execute(sock, m, args) {
         try {
-            let versionData = {};
-            if (fs.existsSync(versionFilePath)) {
-                const rawData = fs.readFileSync(versionFilePath);
-                versionData = JSON.parse(rawData);
-            } else {
-                versionData.versions = {};
-            }
+            const rawdata = fs.readFileSync(versionFile);
+            const versions = JSON.parse(rawdata);
 
-            if (args[0] && args[0].toLowerCase() === 'list') {
-                let versionsMessage = 'Lista de versiones:\n';
-                for (const category in versionData.versions) {
-                    versionsMessage += `**${category}**:\n`;
-                    for (const version of versionData.versions[category]) {
-                        versionsMessage += `  - ${version.version} (${version.date})\n`;
+            if (args[0] === 'list') {
+                const versionList = Object.entries(versions.versions).map(([key, value]) => ({ type: key, data: value }));
+                let response = 'Lista de versiones:\n';
+                
+                for (const entry of versionList) {
+                    response += `\n${entry.type}:\n`;
+                    for (const version of entry.data) {
+                        response += `- ${version.version} (${version.date})\n`;
                     }
                 }
-                sock.sendMessage(m.chat, { text: versionsMessage }, { quoted : m });
+
+                await sock.sendMessage(m.chat, { text: response },  { quoted: m });
             } else {
-                const currentVersions = versionData.versions.newdate || [];
-                const latestVersion = currentVersions.length > 0 ? currentVersions[currentVersions.length - 1] : { version: 'N/A', date: 'N/A' };
-                sock.sendMessage(m.chat, { text: `Versión actual: ${latestVersion.version} (${latestVersion.date})` }, { quoted : m });
+                const currentVersions = versions.versions.newdate;
+                const latestVersion = currentVersions[currentVersions.length - 1];
+
+                await sock.sendMessage(m.chat, { text: `Versión actual: ${latestVersion.version} (${latestVersion.date})` },  { quoted: m });
             }
         } catch (error) {
-            console.error(`Error al leer o escribir en el archivo de versiones: ${error}`);
-            sock.sendMessage(m.chat, { text: 'Ocurrió un error al obtener la versión.' }, { quoted : m });
+            console.error('Error al leer o escribir en el archivo de versiones:', error);
+            const errorMessage = 'Ocurrió un error al procesar la solicitud.';
+            await sock.sendMessage(m.chat, { text: errorMessage },  { quoted: m });
         }
     }
 };
