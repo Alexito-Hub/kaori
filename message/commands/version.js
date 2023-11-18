aff6717e8dedccd4b3e19f838a8b7026d5988445
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-18 00:08:44 -0500

    update version.js

--- message/commands/version.js
+++ message/commands/version.js
@@ -11,23 +11,27 @@ module.exports = {
     async execute(sock, m, args) {
         try {
             const versionFilePath = path.join(__dirname, '../version.json');
+            const defaultVersionData = { versions: { new: [], ancient: [] } };
 
             if (!fs.existsSync(versionFilePath)) {
-                // Si el archivo no existe, crea uno con un arreglo vacío
-                fs.writeFileSync(versionFilePath, '[]', 'utf-8');
+                // Si el archivo no existe, crea uno con datos predeterminados
+                fs.writeFileSync(versionFilePath, JSON.stringify(defaultVersionData, null, 2), 'utf-8');
             }
 
+            const versionData = require('../version.json');
+
             if (args[0] === 'list') {
-                const versions = require('../version.json');
-                const versionList = versions.map(version => `${version.number} - ${version.date}`).join('\n');
-                await sock.sendMessage(m.chat, { text: `Lista de versiones:\n${versionList}` }, {quoted:m});
+                const versionList = versionData.versions.new.concat(versionData.versions.ancient)
+                    .map(version => `${version.version} - ${version.date}`)
+                    .join('\n');
+                await sock.sendMessage(m.chat, { text: `Lista de versiones:\n${versionList}` }, { quoted : m });
             } else {
-                const latestVersion = require('../version.json').slice(-1)[0];
-                await sock.sendMessage(m.chat, { text: `Versión actual: ${latestVersion.number} - ${latestVersion.date}` }, {quoted:m});
+                const latestVersion = versionData.versions.new.slice(-1)[0];
+                await sock.sendMessage(m.chat, { text: `Versión actual: ${latestVersion.version} - ${latestVersion.date}` }, { quoted : m });
             }
         } catch (error) {
             console.error(`Error al leer o escribir en el archivo de versiones: ${error}`);
-            await sock.sendMessage(m.chat, { text: 'Sin Versiones por aqui.' }, {quoted: m});
+            await sock.sendMessage(m.chat, { text: 'Ocurrió un error al obtener la información de la versión.' }, { quoted : m });
         }
     },
 };
