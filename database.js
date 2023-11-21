bd00d519edd1debc5b02ad5040f12890fa61e1f0
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-21 14:00:50 -0500

    update database.js

--- database.js
+++ database.js
@@ -1,18 +1,26 @@
-;( async () => {
-    const JSONPreset = await import("lowdb/node")
-    const fs = require('fs')
-    const path = require('path');
-    
+;(async () => {
+  try {
+    const JSONPreset = await import("lowdb/node");
+    const fs = require("fs");
+    const path = require("path");
+
     const configData = {
-        config: {
-            prefixes: ['#'],
-            owner: ['51968374620'],
-            staff:[]
-        }
+      config: {
+        prefixes: ["#"],
+        owner: ["51968374620"],
+        staff: [],
+      },
+    };
+
+    const dbPath = path.resolve(__dirname, "db.json");
+    if (!fs.existsSync(dbPath)) {
+      fs.writeFileSync(dbPath, JSON.stringify(configData));
     }
-    
-    const db = async () =>  await JSONPreset('db.json', configData)
-    async () => await db.write()
+    const db = await JSONPreset(dbPath);
+    db.defaults(configData).write();
 
     module.exports = db;
-})()
+  } catch (error) {
+    console.error(error);
+  }
+})();
