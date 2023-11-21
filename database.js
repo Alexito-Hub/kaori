f5f3524ddbaf5bff67aaf7ea685f5eeec5b2eed9
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-20 23:39:02 -0500

    update database.js

--- database.js
+++ database.js
@@ -1,15 +1,16 @@
-import('lowdb/node')
-
-const configData = {
-    config: {
-        prefixes: ['#'],
-        owner: ['51968374620'],
-        staff:[]
+;( async () => {
+    const JSONPreset = await import("lowdb/node")
+    
+    const configData = {
+        config: {
+            prefixes: ['#'],
+            owner: ['51968374620'],
+            staff:[]
+        }
     }
-}
-
-
-const db = async (sock) =>  await JSONPreset('db.json', configData)
-async (sock) => await db.write()
+    
+    const db = async (sock) =>  await JSONPreset('db.json', configData)
+    async (sock) => await db.write()
 
 module.exports = db;
+})
