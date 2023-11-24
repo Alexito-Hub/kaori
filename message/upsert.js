b5878c71218f8a1b4ede0098831ab1e4b8693349
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-24 01:50:47 -0500

    update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -33,7 +33,6 @@ module.exports = async(sock, m, store) => {
 		sock = client(sock)
 		v = await sms(sock, m)
 		const db = getDatabase();
-		const updateDb = updateDatabase()
 		const prefix = db.prefixes
 		const prefixes = db.prefixes || ['#'];
 		const isCmd = prefixes.some(prefix => m.body.toLowerCase().startsWith(prefix.toLowerCase()))
