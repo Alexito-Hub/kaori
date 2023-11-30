1741022a166f6723728a68d7055b7d7cf98f548a
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-30 17:21:13 -0500

    update calculadora.js

--- message/commands/calculadora.js
+++ message/commands/calculadora.js
@@ -12,7 +12,17 @@ module.exports = {
             const result = math.evaluate(input);
             sock.sendMessage(m.chat, { text:`Resultado: ${result}`}, { quoted: m });
         } catch (error) {
-            sock.sendMessage(m.chat, { text:`*<cal <expresión matemática>>*`}, { quoted: m });
+            sock.sendMessage(m.chat, { text:`*Uso correcto de la calculadora*
+            
+*Álgebra:* cal solve('2x - 4 = 0')
+*Uso básico:* cal 5 + 3
+*Números grandes:* cal pow(10, 20)
+*Encadenamiento:* cal 2 + 3 * 4
+*Números complejos:* cal sqrt(-1)
+*Expresiones:* cal pow(2, 3) + sqrt(16)
+*Fracciones:* cal 1/3 + 1/4
+
+Recuerda: si no tienes conocimientos, no lo uses`}, { quoted: m });
         }
     }
 }
