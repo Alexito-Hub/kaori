c6fab05531f21a5a6bd5d65a128a39566b9e59c9
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-30 18:37:07 -0500

    update traductor.js

--- message/commands/traductor.js
+++ message/commands/traductor.js
@@ -4,13 +4,14 @@ const translate = new Translate();
 module.exports = {
   name: 'traducir',
   description: 'Traduce un mensaje al idioma especificado.',
-  execute: async (sock, m, args) => {
+  aliases: ['traducir', 'translate', 'tl'],
+  async execute(sock, m, args) {
     try {
       const targetLanguage = args[0];
       const textToTranslate = m.quoted ? m.quoted.body : m.body;
 
       if (!targetLanguage || !textToTranslate) {
-        sock.sendMessage(m.chat, 'Uso correcto: traducir <idioma> <texto>', { quoted: m });
+        sock.sendMessage(m.chat, { text:'traducir <idioma> <texto>'}, { quoted: m });
         return;
       }
 
