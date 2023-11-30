b0862f52ff6756c210a7e1637770c857113fa9dc
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-11-30 16:20:30 -0500

    update calculadora.js

--- message/commands/calculadora.js
+++ message/commands/calculadora.js
@@ -1,5 +1,23 @@
 module.exports = {
     name: 'calculadora',
     description: 'Una calculadora simple y eficaz',
-    aliases: ['cal', 'calculadora', 'math']
+    aliases: ['cal', 'calculadora', 'math'],
+    
+    async execute(sock, m) {
+        const mathExpression = body.slice(body.indexOf(' ') + 1).trim();
+        try {
+            const result = parser.evaluate(mathExpression);
+            sock.sendMessage(m.chat, { text:`🔢 *Operación matemática:*\n${mathExpression}\n🎯 *Resultado:*\n${result}`},{quoted:m})
+            } catch (error) {
+            sock.sendMessage(m.chat, {text:`*Uso correcto: <comando> <operación matemática>*
+
+*Ejemplo:*
+
+ => math 5 + 3
+ => math 10 * 2
+ => math (8 - 3) * 4
+
+*Tambien se puede usar:* mat, mate.`}, {quoted:m})
+            }
+    }
 }
