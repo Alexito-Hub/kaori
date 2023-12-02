5e0d54cf9254eb439218d484683fa13712d6116c
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-01 23:18:43 -0500

    update mathgame.js

--- message/commands/mathgame.js
+++ message/commands/mathgame.js
@@ -7,35 +7,26 @@ module.exports = {
 
     async execute(sock, m) {
         try {
-            // Genera dos números aleatorios para la pregunta
             const num1 = getRandomInt(1, 100);
             const num2 = getRandomInt(1, 100);
 
-            // Decide si la pregunta será de suma o resta
             const isAddition = Math.random() < 0.5;
             const operator = isAddition ? '+' : '-';
 
-            // Calcula la respuesta correcta
             const correctAnswer = isAddition ? num1 + num2 : num1 - num2;
 
-            // Construye el mensaje de la pregunta
             const questionMessage = `¡Hola! Responde la siguiente pregunta:\n\n${num1} ${operator} ${num2} = ?`;
 
-            // Utiliza m.reply para enviar la pregunta al remitente
-            await sock.sendMessage(m.chat, { text: questionMessage }, { quoted: m });
-
-            // Espera la respuesta del usuario
-            const response = await sock.waitForMessage({ sender: m.sender, quoted: m });
-
-            // Verifica si la respuesta es correcta
+            sock.sendMessage(m.chat, { text: questionMessage }, { quoted: m });
+            
+            const response = sock.waitForMessage({ sender: m.sender, quoted: m });
             if (response && parseInt(response.text) === correctAnswer) {
-                // Respuesta correcta
-                await sock.sendMessage(m.chat, { text: `¡Correcto! 🎉 La respuesta es ${correctAnswer}.`}, { quoted: response });
+                sock.sendMessage(m.chat, { text: `¡Correcto! 🎉 La respuesta es ${correctAnswer}.`}, { quoted: response });
             } else {
-                // Respuesta incorrecta
-                await sock.sendMessage(m.chat, { text:`Incorrecto 😟 La respuesta correcta es ${correctAnswer}.`}, { quoted: response });
+                sock.sendMessage(m.chat, { text:`Incorrecto 😟 La respuesta correcta es ${correctAnswer}.`}, { quoted: response });
             }
         } catch (error) {
+            v.reply(`Error ${error}`)
             console.error('Error en la ejecución del comando mathgame:', error);
         }
     }
