2a4e3f74f1a6dcb7195a7ef334dcfb9a8ff4ca7f
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-04 00:01:22 -0500

    update server.js

--- message/commands/server.js
+++ message/commands/server.js
@@ -23,20 +23,20 @@ module.exports = {
             const formattedResponseTime = (responseTime / 1000).toFixed(3);
             
             const serverInfo = {
-                '𝙸𝙳': `${process.pid}`,
+                '𝙸𝙳': `*${process.pid}*`,
                 '𝙾𝚂': `*${os.type()}*`,
                 '𝙰𝚛𝚌𝚑𝚞𝚝𝚎𝚌𝚝𝚞𝚛𝚎': `*${os.arch()}*`,
                 '𝚂𝚝𝚘𝚛𝚊𝚐𝚎': `*${await getStorageInfo()}*`,
                 '𝚁𝙰𝙼': `*${await getRamUsage()}*`,
                 '𝙽𝚘𝚍𝚎 𝚓𝚜': `*${process.version}*`,
                 '𝚁𝚞𝚗𝚝𝚒𝚖𝚎': `*[ ${formattedTime} ]*`,
-                '𝚂𝚙𝚎𝚎𝚍': `${formattedResponseTime} ms`,
-                '𝙾𝚠𝚗𝚎𝚛': `ziooo_zip`
+                '𝚂𝚙𝚎𝚎𝚍': `*${formattedResponseTime} ms*`,
+                '𝙾𝚠𝚗𝚎𝚛': `*ziooo_zip*`
                 
             };
 
             // Construir y enviar el mensaje de información
-            const infoMessage = ` **\n\n╭╶╴╴╴╴╴╴╴╴╴╴╴╴╮\n${formatInfo(serverInfo)}`;
+            const infoMessage = `ㅤ *𝑲𝒂𝒐𝒓𝒊 𝑺𝒕𝒂𝒕𝒖𝒔*\n\n╭╶╴╴╴╴╴╴╴╴╴╴╴╴╮\n${formatInfo(serverInfo)}`;
 
             sock.sendMessage(m.chat, {
                 text: infoMessage,
