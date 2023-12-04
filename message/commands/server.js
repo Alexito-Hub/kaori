a4f00a1eba3ac9f70d65b6f5ec80648842fc9e6c
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-03 23:01:22 -0500

    update server.js

--- message/commands/server.js
+++ message/commands/server.js
@@ -11,33 +11,19 @@ module.exports = {
         try {
             // Información del servidor
             const serverInfo = {
-                'Operador': os.type(),
-                'Sistema Operativo': os.platform(),
-                'Arquitectura': os.arch()
+                '𝙾𝚂': `*${os.type()}*`,
+                '𝙰𝚛𝚚𝚞𝚒𝚝𝚎𝚌𝚝𝚞𝚛𝚊': `*${os.arch()}*`,
+                '𝚁𝚞𝚗𝚝𝚒𝚖𝚎': `*[ ${formatTime(process.uptime())} ]*`,
+                '𝙰𝚕𝚖𝚊𝚌𝚎𝚗𝚊𝚖𝚒𝚎𝚗𝚝𝚘': `*${await getStorageInfo()}*`,
+                '𝚁𝙰𝙼': `*${await getRamUsage()}*`
             };
 
-            // Información del almacenamiento en %
-            exec('df -h --output=pcent /', (error, stdout) => {
-                if (!error) {
-                    const storagePercent = stdout.trim().split('\n')[1] || 'No disponible';
-                    serverInfo['Almacenamiento en %'] = storagePercent;
-                }
+            // Construir y enviar el mensaje de información
+            const infoMessage = `🤖 **𝑺𝒆𝒓𝒗𝒆𝒓 𝑺𝒕𝒂𝒕𝒖𝒔**\n\n${formatInfo(serverInfo)}`;
 
-                // Información del uso de RAM
-                const totalMemory = os.totalmem();
-                const freeMemory = os.freemem();
-                const usedMemory = totalMemory - freeMemory;
-                const usedMemoryPercent = (usedMemory / totalMemory * 100).toFixed(2) + '%';
-                serverInfo['RAM Usado'] = usedMemoryPercent;
-
-                // Construir y enviar el mensaje de información
-                const infoMessage = `🤖 **Información del Servidor y Estado del Bot** 🖥️\n\n` +
-                    `**Información del Servidor**:\n${formatInfo(serverInfo)}`;
-
-                sock.sendMessage(m.chat, { text: infoMessage }, { quoted: m });
-            });
+            sock.sendMessage(m.chat, { text: infoMessage }, { quoted: m });
         } catch (error) {
-            console.log('Error en la ejecución del comando serverinfo:', error);
+            console.error('Error en la ejecución del comando serverinfo:', error);
         }
     }
 };
@@ -45,6 +31,41 @@ module.exports = {
 // Función para formatear la información en un objeto
 function formatInfo(infoObj) {
     return Object.entries(infoObj)
-        .map(([key, value]) => `• ${key}: ${value}`)
-        .join('\n');
+        .map(([key, value]) => `│ *${key}:*\n${value}`)
+        .join('\n├╶╴╴╴╴╴╴╴╴╴╴╴╴┤\n') + '\n╰╶╴╴╴╴╴╴╴╴╴╴╴╴╯';
+}
+
+// Función para formatear el tiempo de actividad (uptime)
+function formatTime(uptimeSeconds) {
+    const days = Math.floor(uptimeSeconds / (24 * 60 * 60));
+    const hours = Math.floor((uptimeSeconds % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((uptimeSeconds % (60 * 60)) / 60);
+    const seconds = uptimeSeconds % 60;
+
+    return `${days > 0 ? `${days}d ` : ''}${hours}h ${minutes}m ${seconds}s`;
+}
+
+// Función para obtener información del almacenamiento
+async function getStorageInfo() {
+    return new Promise((resolve, reject) => {
+        exec('df -h --output=pcent /', (error, stdout) => {
+            if (!error) {
+                const storagePercent = stdout.trim().split('\n')[1] || 'No disponible';
+                resolve(storagePercent);
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
+
+// Función para obtener información del uso de RAM
+async function getRamUsage() {
+    return new Promise((resolve, reject) => {
+        const totalMemory = os.totalmem();
+        const freeMemory = os.freemem();
+        const usedMemory = totalMemory - freeMemory;
+        const usedMemoryPercent = (usedMemory / totalMemory * 100).toFixed(2) + '%';
+        resolve(usedMemoryPercent);
+    });
 }
