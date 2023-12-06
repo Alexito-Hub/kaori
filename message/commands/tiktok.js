1bddbda9ae182be3c6ba78d83b5618c910cad05a
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-06 14:08:17 -0500

    update tiktok.js

--- message/commands/tiktok.js
+++ message/commands/tiktok.js
@@ -4,7 +4,7 @@ const lastDownloads = new Map();
 
 module.exports = {
   name: 'tiktok',
-  description: 'Descarga un video de TikTok sin marca de agua',
+  description: 'Descarga un video o imagen de TikTok sin marca de agua',
   aliases: ['tiktokdownload', 'tt'],
 
   async execute(sock, m) {
@@ -16,7 +16,7 @@ module.exports = {
         const tiktokUrl = m.body.split(' ')[1].trim();
         await tiktokDownloader(sock, m, tiktokUrl);
       } else {
-        v.reply('Por favor, proporciona un enlace de TikTok para descargar el video.', { quoted: m });
+        v.reply('*[ tiktok <url> ]*', { quoted: m });
       }
     } catch (error) {
       console.log('Error en la ejecución del comando tiktok:', error);
@@ -24,29 +24,47 @@ module.exports = {
   }
 };
 
+// Función para descargar contenido de TikTok (video o imagen)
 const tiktokDownloader = async (sock, m, tiktokUrl) => {
   try {
+    // Verifica si ya se descargó un contenido para este chat
     if (lastDownloads.has(m.chat)) {
       v.reply('Espera un momento antes de realizar otra descarga.', { quoted: m });
       return;
     }
+
+    // Almacena el tiempo de la última descarga
     lastDownloads.set(m.chat, Date.now());
 
     const apiUrl = `https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`;
     const response = await fetchJson(apiUrl);
 
-    if (response && response.result && response.result.video && response.result.video.noWatermark) {
-      sock.sendMessage(m.chat, {
-        video: { url: response.result.video.noWatermark },
-        mimetype: 'video/mp4',
-        caption: 'Descargado desde Kaori'
-      }, { quoted: m });
+    if (response && response.result) {
+      const contentType = response.result.type;
+
+      if (contentType === 'video' && response.result.video && response.result.video.noWatermark) {
+        // Envía el video solo si la descarga fue exitosa
+        sock.sendMessage(m.chat, {
+          video: { url: response.result.video.noWatermark },
+          mimetype: 'video/mp4',
+          caption: 'Descargado desde Bot Kaori'
+        }, { quoted: m });
+      } else if (contentType === 'image' && response.result.image) {
+        // Envía la imagen solo si la descarga fue exitosa
+        sock.sendMessage(m.chat, {
+          image: { url: response.result.image },
+          mimetype: 'image/jpeg',
+          caption: 'Descargado desde Bot Kaori'
+        }, { quoted: m });
+      } else {
+        console.log('Tipo de contenido no admitido o error en la descarga');
+        v.reply('Contenido no admitido', { quoted: m });
+      }
     } else {
-      console.log('Error obteniendo información del video de TikTok');
-      v.reply('Error al obtener información del video de TikTok.', { quoted: m });
+      console.log('Error obteniendo información del contenido de TikTok');
     }
   } catch (error) {
-    console.error('Error en la función tiktokDownloader:', error);
-    v.reply('Error al procesar la solicitud. Intenta de nuevo más tarde.', { quoted: m });
+    console.log('Error en la función tiktokDownloader:', error);
+    v.reply('No se pudo procesar la descarga.', { quoted: m });
   }
 };
