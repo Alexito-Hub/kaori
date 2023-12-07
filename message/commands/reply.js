32fa7dcf8acf7e4d99756e39898e5125b6632ae1
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-07 09:27:22 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -1,46 +1,23 @@
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
+// Comando Tag para reenviar mensajes
 module.exports = {
-    name: 'broadcast',
-    description: 'Envía un mensaje a todos los grupos en los que el bot es administrador',
-    aliases: ['bcgc', 'bcgroup'],
+    name: 'tag',
+    description: 'Reenvía el mensaje, incluyendo texto, audio o video.',
+    aliases: ['tag'],
 
-    async execute(sock, m, args) {
+    async execute(sock, m) {
         try {
-            const isOwner = global.owner.includes(m.sender.split('@')[0]);
-
-            if (!isOwner) return v.reply(`Este comando solo puede ser utilizado por el propietario.`, {quoted: m});
-
-            const text = args.join(' ');
-            if (!text) return v.reply(`¿Qué mensaje deseas enviar a los grupos?`, {quoted: m});
-
-            const adminGroups = await sock.getGroupAdmins();
-            
-            if (!adminGroups || adminGroups.length === 0) {
-                return v.reply(`El bot no es administrador en ningún grupo actualmente.`, {quoted:m});
+            // Verifica si el mensaje tiene contenido multimedia (audio o video)
+            if (m.hasMedia) {
+                const mediaData = await sock.downloadMediaMessage(m);
+                // Reenvía el contenido multimedia
+                await sock.sendMessage(m.chat, { media: mediaData }, m);
+            } else {
+                // Reenvía el mensaje de texto si no hay contenido multimedia
+                await sock.sendMessage(m.chat, { text: m.text }, m);
             }
-
-            v.reply(`Enviando mensaje a ${adminGroups.length} Grupos. Tiempo Estimado: ${adminGroups.length * 1.5} segundos.`, {quoted:m});
-
-            for (const groupId of adminGroups) {
-                try {
-                    await sleep(1500);
-                    
-                    if (m.hasMedia) {
-                        const mediaData = await sock.downloadMediaMessage(m);
-                        await sock.sendMessage(groupId, { text, media: mediaData }, m);
-                    } else {
-                        await sock.sendMessage(groupId, { text });
-                    }
-                } catch (sendError) {
-                    console.log(`Error al enviar mensaje al grupo ${groupId}:`, sendError);
-                }
-            }
-
-            v.reply(`Mensaje enviado correctamente a ${adminGroups.length} grupos.`, {quoted:m});
         } catch (error) {
-            console.error('Error en la ejecución del comando broadcast:', error);
-            v.reply(`Se produjo un error al ejecutar el comando broadcast.`, {quoted:m});
+            console.error('Error en la ejecución del comando tag:', error);
+            sock.reply(`Se produjo un error al ejecutar el comando tag.`);
         }
     }
 };
