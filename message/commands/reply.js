7b73d663d257cd792c4bf7d29d4e157d422c97dd
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-06 22:29:40 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -1,16 +1,31 @@
+// Comando para enviar un mensaje a todos los grupos
+const sendMessageToAllGroups = async (sock, message) => {
+    try {
+        const groups = await sock.getAllGroups();
+        for (const group of groups) {
+            await sock.sendMessage(group.jid, { text: message });
+            // Agrega un pequeño retraso para evitar posibles restricciones
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+        sock.reply(sock.decodeJid(sock.user.jid), 'Mensaje enviado a todos los grupos.');
+    } catch (error) {
+        console.error('Error al enviar mensaje a todos los grupos:', error);
+        sock.reply(sock.decodeJid(sock.user.jid), 'Error al enviar mensaje a todos los grupos.');
+    }
+};
+
 module.exports = {
-    name: 'replicar',
-    description: 'Replique o reenvíe un mensaje en todos los grupos',
-    aliases: ['replall', 'enviaratodos'],
+    name: 'sendallgroups',
+    description: 'Envía un mensaje a todos los grupos',
+    aliases: ['sendgroups', 'broadcast'],
 
-    execute(sock, m, args) {
+    async execute(sock, m, args) {
+        if (args.length !== 1) {
+            v.reply('Uso incorrecto. Proporcione el mensaje que desea enviar a todos los grupos.');
+            return;
+        }
 
-        const messageToReplicate = args.join(' ');
-
-        const allGroups = sock.chats.filter(chat => chat.jid.endsWith('@g.us') && chat.name).map(chat => chat.jid);
-
-        allGroups.forEach(group => {
-            sock.sendMessage(group, { text: messageToReplicate }, { quoted: m });
-        });
+        const message = args[0];
+        sendMessageToAllGroups(sock, message);
     }
 };
