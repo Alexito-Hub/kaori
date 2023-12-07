621bcea4775d7fdbd3d689f590a36f1296a4f6fb
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-07 00:59:45 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -2,34 +2,45 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = {
     name: 'broadcast',
-    description: 'Envía un mensaje a todos los grupos en los que el bot está participando',
+    description: 'Envía un mensaje a todos los grupos en los que el bot es administrador',
     aliases: ['bcgc', 'bcgroup'],
 
     async execute(sock, m, args) {
         try {
-
             const isOwner = global.owner.includes(m.sender.split('@')[0]);
 
-            if (!isOwner) return v.reply(`Este comando solo puede ser utilizado por el propietario.`);
+            if (!isOwner) return v.reply(`Este comando solo puede ser utilizado por el propietario.`, {quoted: m});
 
             const text = args.join(' ');
-            if (!text) return v.reply(`¿Qué mensaje deseas enviar a todos los grupos?`);
+            if (!text) return v.reply(`¿Qué mensaje deseas enviar a los grupos?`, {quoted: m});
 
-            const groups = await sock.groupFetchAllParticipating();
-            const groupIds = Object.keys(groups);
-
-
-            v.reply(`Enviando mensaje a ${groupIds.length} Grupos. Tiempo Estimado: ${groupIds.length * 1.5} segundos.`);
-
-            for (const groupId of groupIds) {
-                await sleep(1500);
-                // Envia el mensaje a cada grupo
-                sock.sendMessage(groupId, { text });
+            const adminGroups = await sock.getGroupAdmins();
+            
+            if (!adminGroups || adminGroups.length === 0) {
+                return v.reply(`El bot no es administrador en ningún grupo actualmente.`, {quoted:m});
             }
 
-            v.reply(`Mensaje enviado correctamente a ${groupIds.length} grupos.`);
+            v.reply(`Enviando mensaje a ${adminGroups.length} Grupos. Tiempo Estimado: ${adminGroups.length * 1.5} segundos.`, {quoted:m});
+
+            for (const groupId of adminGroups) {
+                try {
+                    await sleep(1500);
+                    
+                    if (m.hasMedia) {
+                        const mediaData = await sock.downloadMediaMessage(m);
+                        await sock.sendMessage(groupId, { text, media: mediaData }, m);
+                    } else {
+                        await sock.sendMessage(groupId, { text });
+                    }
+                } catch (sendError) {
+                    console.log(`Error al enviar mensaje al grupo ${groupId}:`, sendError);
+                }
+            }
+
+            v.reply(`Mensaje enviado correctamente a ${adminGroups.length} grupos.`, {quoted:m});
         } catch (error) {
-            console.log('Error en la ejecución del comando broadcast:', error);
+            console.error('Error en la ejecución del comando broadcast:', error);
+            v.reply(`Se produjo un error al ejecutar el comando broadcast.`, {quoted:m});
         }
     }
 };
