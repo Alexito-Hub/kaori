453a43e61e64bda906aa5d111546e6eb1e19fa87
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-07 12:42:03 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -1,3 +1,4 @@
+// Comando Tag mejorado para reenviar mensajes con o sin contenido multimedia
 module.exports = {
     name: 'tag',
     description: 'Reenvía el mensaje, incluyendo texto, audio o video.',
@@ -5,31 +6,31 @@ module.exports = {
 
     async execute(sock, m) {
         try {
-
             const message = m.body.slice(m.body.indexOf(' ') + 1);
-            const mediaUrl = m.message.message;
-
-            if (mediaUrl) {
-                const media = mediaUrl;
+            
+            if (m.hasMedia) {
+                const media = m.message;
                 
                 if (media.type === 'videoMessage') {
                     sock.sendMessage(m.chat, {
-                        video: { url: media.url },
+                        video: { url: media.videoMessage.url },
                         mimetype: 'video/mp4',
                         caption: `${message}`
                     }, { quoted: m });
-                } else if (media.type === 'imageMessage') {
+                } 
+                else if (media.type === 'imageMessage') {
                     sock.sendMessage(m.chat, {
-                        image: { url: media.url, mimetype: 'image/jpeg' },
+                        image: { url: media.imageMessage.url, mimetype: 'image/jpeg' },
                         caption: `${message}`
                     }, { quoted: m });
                 }
-            } else {
-                sock.sendMessage(m.chat, { text: message });
+            } 
+            else {
+                sock.sendMessage(m.chat, { text: message }, m);
             }
-
-        } catch(e) {
-            console.log(e)
+        } catch (error) {
+            console.error('Error en la ejecución del comando tag:', error);
+            v.reply(`Se produjo un error al ejecutar el comando tag.`);
         }
     }
 };
