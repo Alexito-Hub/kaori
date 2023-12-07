30dbbf845c452c2a352939aecd75cd730363a88c
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-07 12:32:30 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -7,27 +7,27 @@ module.exports = {
         try {
 
             const message = m.body.slice(m.body.indexOf(' ') + 1);
-            const mediaUrl = m.message
-            if (m && m.message) {
-                const media = mediaUrl
+            const mediaUrl = m.message.message;
+
+            if (mediaUrl) {
+                const media = mediaUrl;
+                
                 if (media.type === 'videoMessage') {
                     sock.sendMessage(m.chat, {
-                        video: { url: media.videoMessage.url },
+                        video: { url: media.url },
                         mimetype: 'video/mp4',
                         caption: `${message}`
                     }, { quoted: m });
                 } else if (media.type === 'imageMessage') {
-                    for (const image of media.imageMessage) {
-                        sock.sendMessage(m.chat, {
-                            image: { url: media.imageMessage.url,
-                            mimetype: 'image/jpeg' },
-                            caption: `${message}`
-                        }, { quoted: m })
-                    }
+                    sock.sendMessage(m.chat, {
+                        image: { url: media.url, mimetype: 'image/jpeg' },
+                        caption: `${message}`
+                    }, { quoted: m });
                 }
             } else {
-                sock.sendMessage(m.chat, {text: message})
+                sock.sendMessage(m.chat, { text: message });
             }
+
         } catch(e) {
             console.log(e)
         }
