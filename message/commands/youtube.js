407ea06c2ad085f19468ed2436731244c14fe962
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-09 11:26:57 -0500

    update youtube.js

--- message/commands/youtube.js
+++ message/commands/youtube.js
@@ -1,4 +1,5 @@
 const ytdl = require('ytdl-core')
+const sizeLimit = 100 * 1024 * 1024
 
 module.exports = {
     name: 'youtube',
@@ -12,23 +13,71 @@ module.exports = {
         }
 
         const youtubeUrl = args[0];
-        downloadYoutubeVideo(sock, m, youtubeUrl);
+
+        if (await validateUrl(youtubeUrl)) {
+            const video = await downloadYoutubeVideo(youtubeUrl);
+
+            await sendVideo(sock, m, video);
+        } else {
+            v.reply('URL de YouTube no válida.');
+        }
     }
 };
 
-const downloadYoutubeVideo = async (sock, m, url) => {
-    try {
-        const info = await ytdl.getInfo(url);
-        const format = ytdl.chooseFormat(info.formats, { quality: 'highestvideo' });
+const validateUrl = async (url) => {
+    if (ytdl.validateURL(url)) {
+        const videoId = ytdl.getURLVideoID(url);
 
-        if (format) {
-            // Envía el video al usuario
-            sock.sendMessage(m.chat, { video: { url: format.url }, mimetype: 'video/mp4', caption: 'Video descargado de YouTube' }, { quoted: m });
+        if (ytdl.validateID(videoId)) {
+            return true;
         } else {
-            v.reply('Formato de video no válido.');
+            v.reply('Error al obtener información.');
+            return false;
         }
-    } catch (error) {
-        console.error('Error al obtener información de YouTube:', error);
-        v.reply('Error al obtener información de YouTube.');
+    } else {
+        v.reply('URL de YouTube no válida.');
+        return false;
+    }
+};
+
+const downloadYoutubeVideo = async (url) => {
+    const info = await ytdl.getInfo(url);
+
+    const format = ytdl.chooseFormat(info.formats, { quality: 'highestvideo', filter: format => format.container === 'mp4' });
+
+    let buffer = Buffer.alloc(0);
+
+    let canceled = false;
+
+    const stream = ytdl.downloadFromInfo(info, { format: format });
+
+    stream.on('progress', (chunkLength, downloaded, total) => {
+        buffer = Buffer.concat([buffer, stream.read(chunkLength)]);
+
+        if (buffer.length > sizeLimit) {
+            stream.destroy();
+
+            v.reply('El video supera el límite de 100 MB.');
+
+            canceled = true;
+        }
+    });
+
+    await new Promise((resolve, reject) => {
+        stream.on('end', resolve);
+        stream.on('error', reject);
+    });
+
+    if (canceled) {
+        return null;
+    } else {
+        return { buffer: buffer, mimetype: 'video/mp4', caption: 'Video descargado de YouTube' };
+    }
+};
+
+const sendVideo = async (sock, m, video) => {
+    if (video) {
+
+        sock.sendMessage(m.chat, { video: video }, { quoted: m });
     }
 };
