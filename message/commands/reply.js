1d8ac9c900d01ef921c9744a07cf4d211ec800d2
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-08 21:19:17 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -1,30 +1,34 @@
-const isOwner = require('../../message/upsert')
+const { owner } = require('../config'); // Asegúrate de tener configurada la variable 'owner' en tu archivo de configuración
+
 module.exports = {
     name: 'tagall',
     description: 'Etiqueta a todos los miembros de los grupos en los que participa el bot',
     aliases: ['tag', 'todos', 'all'],
 
-    async execute(sock, m) {
+    async execute(sock, m, args) {
         try {
-            if (isOwner) {
-
-                const groups = await sock.groupFetchAllParticipating();
-                const groupIds = Object.keys(groups);
-                
-                const message = args.join(' ');
-                if (!message) return v.reply('¿Falta de ideas para un mensaje?');
-    
-                for (const groupId of groupIds) {
-                    await sock.sendMessage(groupId, { 
-                        text: message, 
-                        contextInfo: { remoteJid: groupId },
-                    });
-                }
-                await sock.sendMessage(m.chat, { text:'Envio de mensaje correcto.'}, { quoted: m });
+            const senderNumber = m.sender.split('@')[0];
+            if (senderNumber !== owner) {
+                return await sock.sendMessage(m.chat, { text: 'Solo el creador puede usar este comando.' }, { quoted: m });
             }
+
+            const groups = await sock.groupFetchAllParticipating();
+            const groupIds = Object.keys(groups);
+
+            const message = args.join(' ');
+            if (!message) return await sock.sendMessage(m.chat, { text: '¿Falta de ideas para un mensaje?' }, { quoted: m });
+
+            for (const groupId of groupIds) {
+                await sock.sendMessage(groupId, { 
+                    text: message, 
+                    contextInfo: { remoteJid: groupId },
+                });
+            }
+
+            await sock.sendMessage(m.chat, { text: 'Envío de mensaje correcto.' }, { quoted: m });
         } catch (error) {
-            console.log(error);
-            await sock.sendMessage(m.chat, {text:'Error al realizar el envio de mensajes'}, { quoted: m });
+            console.error(error);
+            await sock.sendMessage(m.chat, { text: 'Error al realizar el envío de mensajes' }, { quoted: m });
         }
     },
 };
