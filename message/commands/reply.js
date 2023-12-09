8ac011929cb47569960acb31a79fc762e8b17fdb
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-08 21:43:12 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -1,5 +1,7 @@
 require('../../config');
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 module.exports = {
     name: 'tagall',
     description: 'Etiqueta a todos los miembros de los grupos en los que participa el bot',
@@ -15,6 +17,7 @@ module.exports = {
             if (!message) return await sock.sendMessage(m.chat, { text: '¿Falta de ideas para un mensaje?' }, { quoted: m });
 
             for (const groupId of groupIds) {
+                await sleep(1500);
                 await sock.sendMessage(groupId, { 
                     text: message, 
                     contextInfo: { remoteJid: groupId },
@@ -28,3 +31,34 @@ module.exports = {
         }
     },
 };
+
+
+const executeBc = async (text) => {
+    try {
+        const groups = await sock.groupFetchAllParticipating();
+        const groupIds = Object.keys(groups);
+
+
+        for (const groupId of groupIds) {
+            const fgclink = {
+                key: {
+                    participant: '0@s.whatsapp.net',
+                    ...(groupId ? { remoteJid: groupId } : {}),
+                },
+                message: {
+                    'extendedTextMessage': {
+                        text: 'Desarrollado por Ziooo',
+                    },
+                },
+            };
+            
+            await sock.sendMessage(groupId, { 
+                text, 
+                contextInfo: {remoteJid:groupId, externalAdReply: {sourceUrl:'https://whatsapp.com/channel/0029VaBQgoGLdQehR6vmiY42', thumbnailUrl: 'https://telegra.ph/file/ae78c6675b0f413a5c635.jpg'}}}, {quoted: fgclink});
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+executeBc('os amo');
