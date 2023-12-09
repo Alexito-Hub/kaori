0a27579403f84a014c0880e71bb0ae32299128fc
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-08 21:05:14 -0500

    update reply.js

--- message/commands/reply.js
+++ message/commands/reply.js
@@ -1,41 +1,29 @@
-// Comando Tag mejorado para reenviar mensajes con o sin contenido multimedia
 module.exports = {
-    name: 'tag',
-    description: 'Reenvía el mensaje, incluyendo texto, audio o video.',
-    aliases: ['tag'],
+    name: 'tagall',
+    description: 'Etiqueta a todos los miembros de los grupos en los que participa el bot',
+    aliases: ['tag', 'todos', 'all'],
 
-    async execute(sock, m, args) {
+    async execute(sock, m) {
         try {
-            const message = args.join(' ');
-            if (!message) return v.reply('¿Falta de palabras?');
+            if (isOwner) {
 
-            // Verifica si el mensaje tiene contenido multimedia
-            if (m.hasMedia) {
-                const media = m.quotedMsgObj;
-
-                // Maneja mensajes de video
-                if (media.isVideo) {
-                    sock.sendMessage(m.chat, {
-                        video: { url: media.body },
-                        mimetype: 'video/mp4',
-                        caption: `${message}`
-                    }, { quoted: m });
-                } 
-                // Maneja mensajes de imagen
-                else if (media.isImage) {
-                    sock.sendMessage(m.chat, {
-                        image: { url: media.body },
-                        caption: `${message}`
-                    }, { quoted: m });
+                const groups = await sock.groupFetchAllParticipating();
+                const groupIds = Object.keys(groups);
+                
+                const message = args.join(' ');
+                if (!message) return v.reply('¿Falta de ideas para un mensaje?');
+    
+                for (const groupId of groupIds) {
+                    await sock.sendMessage(groupId, { 
+                        text: message, 
+                        contextInfo: { remoteJid: groupId },
+                    });
                 }
-            } 
-            // Si no hay contenido multimedia, reenvía solo el mensaje de texto
-            else {
-                sock.sendMessage(m.chat, { text: message, contextInfo: m.chat});
+                await sock.sendMessage(m.chat, { text:'Envio de mensaje correcto.'}, { quoted: m });
             }
         } catch (error) {
-            console.log('Error en la ejecución del comando tag:', error);
-            v.reply('Se produjo un error al ejecutar el comando tag.');
+            console.log(error);
+            await sock.sendMessage(m.chat, 'Error al realizar el envio de mensajes', { quoted: m });
         }
-    }
+    },
 };
