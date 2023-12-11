1f3ddbd9e402bd0b9110aaa20fa39992ba0adb99
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 00:30:26 -0500

    update tiktok.js

--- message/commands/tiktok.js
+++ message/commands/tiktok.js
@@ -13,30 +13,10 @@ module.exports = {
                 v.reply('*tiktok <url>*');
                 return;
             }
-            sock.sendMessage(m.chat, {react: {text: '🕛',key: m.key,}})
-            const subcommand = args[1].toLowerCase();
-            const tiktokUrl = args[0];
-            const response = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`);
             
-            if (subcommand === 'audio' && tiktokUrl) {
-                if (!args[0]) {
-                    v.reply('*tiktok audio <url>*')
-                }
-                if (response && response.result && response.result.type === 'video') {
-                    const audioUrl = response.result.music.url;
-                    sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
-                    sock.sendMessage(m.chat, {
-                        audio: { url: audioUrl },
-                        mimetype: 'audio/mp4',
-                        ppt: true,
-                    });
-                    return;
-                } else {
-                    console.log('Error al obtener información de TikTok para el subcomando audio');
-                    sock.sendMessage(m.chat, {react: {text: '❎',key: m.key,}})
-                    v.reply('Parece que hubo un problema al obtener el audio, inténtalo de nuevo');
-                    return;
-                }
+            const tiktokUrl = args[0];
+            if (tiktokUrl) {
+                sock.sendMessage(m.chat, {react: {text: '🕛',key: m.key,}})
             }
             
             function roundTime(time) {
@@ -46,35 +26,61 @@ module.exports = {
             const responseMs = Date.now();
             const responseTime = roundTime(responseMs - m.messageTimestamp * 1000);
             const formattedResponseTime = (responseTime / 1000).toFixed(3);
-
-            if (response && response.result) {
-                const result = response.result;
-                if (result.type === 'video') {
+            
+            if (args[1] && args[1].toLowerCase() === 'audio') {
+                if (!args[1]) {
+                    v.reply('*tiktok audio <url>')
+                    return;
+                }
+                const audioResponse = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`);
+                if (audioResponse && audioResponse.result && audioResponse.result.music) {
+                    const audioResult = audioResponse.result.music;
                     sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
                     sock.sendMessage(m.chat, {
-                        video: { url: result.video.noWatermark },
-                        mimetype: 'video/mp4',
-                        caption: `ㅤ *⋯⋯ TIK TOK ⋯⋯*
+                        audio: { url: audioResult.url },
+                        mimetype: 'audio/mp4',
+                        ppt: true,
+                    });
+                } else {
+                    console.log('Error al obtener información del audio de TikTok');
+                    v.reply('Parece que hubo un problema al obtener el audio, inténtalo de nuevo');
+                }
+            } else {
+                const response = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`);
+                if (response && response.result) {
+                    const result = response.result;
+                    if (result.type === 'video') {
+                        sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
+                        sock.sendMessage(m.chat, {
+                            video: { url: result.video.noWatermark },
+                            mimetype: 'video/mp4',
+                            caption: `ㅤ *⋯⋯ TIK TOK ⋯⋯*
  ∘ *Autor:* ${result.author.name}
  ∘ *Likes:* ${result.information.likeCount}
  ∘ *Comentarios:* ${result.information.commentCount}
  ∘ *Fecha:* ${result.information.created_at}
  ∘ *Titulo:* ${result.information.title}`
-                    }, {quoted:m});
-                } else if (result.type === 'images') {
-                    for (const image of result.images) {
-                        sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
-                        sock.sendMessage(m.chat, {
-                            image: { url: image.url.url, mimetype: 'image/jpeg' },
-                            caption: `᳃ ¡Listo! - *🧃 ${formattedResponseTime} ms*`
                         }, {quoted:m});
-          }
-        }
-      } else {
-        console.log('Error al obtener información');
-        sock.sendMessage(m.chat, {react: {text: '❎',key: m.key,}})
-        v.reply(`Hubo un problema al obtener información`);
-      }
+                    } else if (result.type === 'images') {
+                        for (const image of result.images) {
+                            sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
+                            sock.sendMessage(m.chat, {
+                                image: { url: image.url.url, mimetype: 'image/jpeg' },
+                                caption: `᳃ ¡Listo! - *🧃 ${formattedResponseTime} ms*`
+                            }, {quoted:m});
+                            
+                        }
+                        
+                    }
+                    
+                } else {
+                    console.log('Error al obtener información');
+                    sock.sendMessage(m.chat, {react: {text: '❎',key: m.key,}})
+                    v.reply(`Hubo un problema al obtener información`);
+                }
+            }
+
+
     } catch (error) {
       console.log('Error:', error);
       v.reply('Error');
