79738c1bcf317e8e5d1ff8d04fa2f7555c110c04
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 12:52:33 -0500

    update kick.js

--- message/commands/kick.js
+++ message/commands/kick.js
@@ -9,36 +9,33 @@ module.exports = {
       const groupInfo = await sock.groupMetadata(m.chat);
       console.log('Información del grupo:', groupInfo);
 
-      // Verificar si el bot es administrador del grupo
-      const isBotAdmin = groupInfo && groupInfo.owner == sock.user.id;
-
       // Verificar si el remitente del mensaje es administrador del grupo
-      const isSenderAdmin = groupInfo && groupInfo.participants.some(p => p.jid == m.sender && ['admin', 'superadmin'].includes(p.admin));
-      console.log('Es administrador del grupo:', isSenderAdmin);
+      const isAdmin = groupInfo && groupInfo.participants.some(p => p.id == m.sender && ['admin', 'superadmin'].includes(p.admin));
+      console.log('Es administrador del grupo:', isAdmin);
 
       // Verificar si se proporciona un usuario a expulsar
       if (args.length === 0 && !m.quoted) {
-        sock.sendMessage(m.chat, {text:'*kick <@usuario>*'}, { quoted: m });
+        sock.sendMessage(m.chat, '*kick <@usuario>*', { quoted: m });
         return;
       }
 
       // Obtener el ID del usuario a expulsar
       const targetUser = (args.length > 0) ? args[0].replace('@', '').replace(/\s/g, '') + '@s.whatsapp.net' : m.quoted.sender;
 
-      // Verificar si el bot y el remitente del mensaje son administradores
-      if (isBotAdmin && isSenderAdmin) {
+      // Verificar si el remitente del mensaje es administrador
+      if (isAdmin) {
         // Expulsar al usuario del grupo
         await sock.groupRemove(m.chat, [targetUser]);
 
         // Enviar mensaje de éxito
-        sock.sendMessage(m.chat, {text:`Usuario ${targetUser} expulsado del grupo.`}, { quoted: m });
+        sock.sendMessage(m.chat, `Usuario ${targetUser} expulsado del grupo.`, { quoted: m });
       } else {
-        // Enviar mensaje de error si el bot o el remitente no son administradores
-        sock.sendMessage(m.chat, {text:'Solo los administradores pueden expulsar a miembros del grupo.'}, { quoted: m });
+        // Enviar mensaje de error si el remitente no es administrador
+        sock.sendMessage(m.chat, 'Solo los administradores pueden expulsar a miembros del grupo.', { quoted: m });
       }
     } catch (error) {
       console.error('Error:', error);
-      sock.sendMessage(m.chat, {text:'Error al ejecutar el comando'}, { quoted: m });
+      sock.sendMessage(m.chat, 'Error al ejecutar el comando', { quoted: m });
     }
   },
 };
