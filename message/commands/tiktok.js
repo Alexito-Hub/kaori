343d0741c2a0b04a6cdc77086f002b94c656bd6c
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 00:44:52 -0500

    update tiktok.js

--- message/commands/tiktok.js
+++ message/commands/tiktok.js
@@ -1,87 +1,76 @@
-// Importa las funciones necesarias
-const { fetchJson } = require('../../lib/utils')// Ajusta la ruta según la ubicación de tu función fetchJson
+const { fetchJson } = require('../../lib/utils');
 
-// Define el comando
 module.exports = {
-    name: 'tiktok',
-    description: 'Descarga videos e imágenes de TikTok',
-    aliases: ['tiktok', 'tt'],
-    
-    async execute(sock, m, args) {
-        try {
-            if (!args[0]) {
-                v.reply('*tiktok <url>*');
-                return;
-            }
-            
-            const tiktokUrl = args[0];
-            if (tiktokUrl) {
-                sock.sendMessage(m.chat, {react: {text: '🕛',key: m.key,}})
-            }
-            
-            function roundTime(time) {
-                return Math.round(time);
-            }
-            
-            const responseMs = Date.now();
-            const responseTime = roundTime(responseMs - m.messageTimestamp * 1000);
-            const formattedResponseTime = (responseTime / 1000).toFixed(3);
-            
-            if (args[1] && args[1].toLowerCase() === 'audio') {
-                if (!args[0]) {
-                    v.reply('*tiktok audio <url>')
-                    return;
-                }
-                const tiktokAudioUrl = args[0]
-                const audioResponse = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokAudioUrl}&apikey=StarAPI`);
-                if (audioResponse && audioResponse.result && audioResponse.result.music) {
-                    const audioResult = audioResponse.result.music;
-                    sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
-                    sock.sendMessage(m.chat, {
-                        audio: { url: audioResult.url },
-                        mimetype: 'audio/mp4',
-                        ppt: true,
-                    });
-                } else {
-                    console.log('Error al obtener información del audio de TikTok');
-                    v.reply('Parece que hubo un problema al obtener el audio, inténtalo de nuevo');
-                }
-            } else {
-                const response = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`);
-                if (response && response.result) {
-                    const result = response.result;
-                    if (result.type === 'video') {
-                        sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
-                        sock.sendMessage(m.chat, {
-                            video: { url: result.video.noWatermark },
-                            mimetype: 'video/mp4',
-                            caption: `ㅤ *⋯⋯ TIK TOK ⋯⋯*
- ∘ *Autor:* ${result.author.name}
- ∘ *Likes:* ${result.information.likeCount}
- ∘ *Comentarios:* ${result.information.commentCount}
- ∘ *Fecha:* ${result.information.created_at}
- ∘ *Titulo:* ${result.information.title}`
-                        }, {quoted:m});
-                    } else if (result.type === 'images') {
-                        for (const image of result.images) {
-                            sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
-                            sock.sendMessage(m.chat, {
-                                image: { url: image.url.url, mimetype: 'image/jpeg' },
-                                caption: `᳃ ¡Listo! - *🧃 ${formattedResponseTime} ms*`
-                            }, {quoted:m});
-                            
-                        }
-                        
-                    }
-                    
-                } else {
-                    console.log('Error al obtener información');
-                    sock.sendMessage(m.chat, {react: {text: '❎',key: m.key,}})
-                    v.reply(`Hubo un problema al obtener información`);
-                }
-            }
+  name: 'tiktok',
+  description: 'Descarga videos e imágenes de TikTok',
+  aliases: ['tiktok', 'tt'],
 
+  async execute(sock, m, args) {
+    try {
+      if (!args[0]) {
+        v.reply('*tiktok <url>*');
+        return;
+      }
 
+      const subCommand = args.shift().toLowerCase();
+
+      if (subCommand === 'audio') {
+        // Lógica para el subcomando "audio"
+        const tiktokUrl = args[0];
+        const response = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`);
+
+        if (response && response.result && response.result.type === 'video') {
+          const audioUrl = response.result.music.url;
+
+          sock.sendMessage(m.chat, {
+            audio: { url: audioUrl },
+            mimetype: 'audio/mp4',
+            ppt: true,
+          });
+        } else {
+          v.reply('No se pudo obtener el audio del video de TikTok');
+        }
+      } else {
+        // Lógica para el comando principal "tiktok"
+        const tiktokUrl = subCommand;
+        const response = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`);
+
+        function roundTime(time) {
+          return Math.round(time);
+        }
+
+        const responseMs = Date.now();
+        const responseTime = roundTime(responseMs - m.messageTimestamp * 1000);
+        const formattedResponseTime = (responseTime / 1000).toFixed(3);
+
+        if (response && response.result) {
+          const result = response.result;
+          sock.sendMessage(m.chat, { react: { text: '🕛', key: m.key } });
+
+          if (result.type === 'video') {
+            sock.sendMessage(m.chat, {
+              video: { url: result.video.noWatermark },
+              mimetype: 'video/mp4',
+              caption: `ㅤ *- - TIK TOK*
+*Autor:* ${result.author.name}
+*Like:* ${result.information.likeCount}
+*Comentarios:* ${result.information.commentCount}
+*Fecha:* ${result.information.created_at}
+*Titulo:* ${result.information.title}`,
+            }, { quoted: m });
+          } else if (result.type === 'images') {
+            for (const image of result.images) {
+              sock.sendMessage(m.chat, {
+                image: { url: image.url.url, mimetype: 'image/jpeg' },
+                caption: `¡Listo! - *🧃 ${formattedResponseTime} ms*`,
+              }, { quoted: m });
+            }
+          }
+        } else {
+          console.log('Error al obtener información de TikTok');
+          v.reply('Parece que hubo un problema, inténtalo de nuevo');
+        }
+      }
     } catch (error) {
       console.log('Error:', error);
       v.reply('Error');
