84076a2dbc4537b48af7451d0269842a65b30626
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 17:29:54 -0500

    update kick.js

--- message/commands/kick.js
+++ message/commands/kick.js
@@ -11,32 +11,10 @@ module.exports = {
             }
 
             const groupInfo = await sock.groupMetadata(m.chat);
-
-            const botNumber = sock.user.id.split(':')[0];
-            const admins = await sock.getGroupAdmins(m.chat);
-
-            const isAdmin = admins.includes(botNumber);
+            const isAdmin = groupInfo && groupInfo.participants.some(p => p.id == sock.user.id && ['admin', 'superadmin'].includes(p.admin));
             
             if (!isAdmin) {
                 sock.sendMessage(m.chat, { text: 'El bot necesita ser administrador del grupo para usar este comando.' }, { quoted: m });
-                return;
-            }
-
-            const senderNumber = m.sender.split('@')[0];
-            const isAdminUser = groupInfo && groupInfo.participants.some(p => p.id == senderNumber && ['admin', 'superadmin'].includes(p.admin));
-            
-            if (!isAdminUser) {
-                sock.sendMessage(m.chat, { text: 'Solo los administradores pueden expulsar a miembros del grupo.' }, { quoted: m });
-                return;
-            }
-
-            let targetUser;
-            if (args.length > 0) {
-                targetUser = args[0].replace('@', '').replace(/\s/g, '').split('@')[0] + '@s.whatsapp.net';
-            } else if (m.quoted) {
-                targetUser = m.quoted.sender;
-            } else {
-                sock.sendMessage(m.chat, { text: 'Usa el formato *kick <@usuario>* o responde al mensaje del usuario que deseas expulsar.' }, { quoted: m });
                 return;
             }
 
@@ -52,6 +30,16 @@ module.exports = {
                 return;
             }
 
+            let targetUser;
+            if (args.length > 0) {
+                targetUser = args[0].replace('@', '').replace(/\s/g, '').split('@')[0] + '@s.whatsapp.net';
+            } else if (m.quoted) {
+                targetUser = m.quoted.sender;
+            } else {
+                sock.sendMessage(m.chat, { text: 'Usa el formato *kick <@usuario>* o responde al mensaje del usuario que deseas expulsar.' }, { quoted: m });
+                return;
+            }
+
             await sock.groupParticipantsUpdate(m.chat, [targetUser], 'remove');
             sock.sendMessage(m.chat, {
                 contextInfo: {
@@ -60,7 +48,7 @@ module.exports = {
                 },
                 video: { url: 'https://telegra.ph/file/25ec490a6f4dd4b423110.mp4' },
                 gifPlayback: true,
-                caption: `Usuario @${targetUser.split('@')[0]} expulsado del grupo por @${senderNumber}`,
+                caption: `Usuario @${targetUser.split('@')[0]} expulsado del grupo por @${m.sender.split('@')[0]}`,
             });
 
         } catch (error) {
