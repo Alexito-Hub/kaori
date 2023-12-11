1bf9d2bb37c949d3d271ef70494bb6cbf23adacd
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 13:25:57 -0500

    update kick.js

--- message/commands/kick.js
+++ message/commands/kick.js
@@ -1,41 +1,31 @@
 module.exports = {
-  name: 'kick',
-  description: 'Expulsa a un miembro del grupo',
-  aliases: ['expulsar'],
-
-  async execute(sock, m, args) {
-    try {
-      // Obtener información del grupo
-      const groupInfo = await sock.groupMetadata(m.chat);
-      console.log('Información del grupo:', groupInfo);
-
-      // Verificar si el remitente del mensaje es administrador del grupo
-      const isAdmin = groupInfo && groupInfo.participants.some(p => p.id == m.sender && ['admin', 'superadmin'].includes(p.admin));
-      console.log('Es administrador del grupo:', isAdmin);
-
-      // Verificar si se proporciona un usuario a expulsar
-      if (args.length === 0 && !m.quoted) {
-        sock.sendMessage(m.chat, {text:'*kick <@usuario>*'}, { quoted: m });
-        return;
-      }
-
-      // Obtener el ID del usuario a expulsar
-      const targetUser = (args.length > 0) ? args[0].replace('@', '').replace(/\s/g, '') + '@s.whatsapp.net' : m.quoted.sender;
-
-      // Verificar si el remitente del mensaje es administrador
-      if (isAdmin) {
-        // Expulsar al usuario del grupo
-        await sock.groupParticipantsUpdate(m.chat, [targetUser], 'remove');
-
-        // Enviar mensaje de éxito
-        sock.sendMessage(m.chat, {text:`Usuario ${targetUser} expulsado del grupo.`}, { quoted: m });
-      } else {
-        // Enviar mensaje de error si el remitente no es administrador
-        sock.sendMessage(m.chat, {text:'Solo los administradores pueden expulsar a miembros del grupo.'}, { quoted: m });
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      sock.sendMessage(m.chat, {text:'Error al ejecutar el comando'}, { quoted: m });
-    }
-  },
+    name: 'kick',
+    description: 'Expulsa a un miembro del grupo',
+    aliases: ['expulsar'],
+    
+    async execute(sock, m, args) {
+        try {
+            const groupInfo = await sock.groupMetadata(m.chat);
+            const isAdmin = groupInfo && groupInfo.participants.some(p => p.id == m.sender && ['admin', 'superadmin'].includes(p.admin));
+            if (args.length === 0 && !m.quoted) {
+                sock.sendMessage(m.chat, {text:'*kick <@usuario>*'}, { quoted: m });
+                return;
+            }
+            
+            const targetUser = (args.length > 0) ? args[0].replace('@', '').replace(/\s/g, '') + '@s.whatsapp.net' : m.quoted.sender;
+            const user = m.sender.split('@')[0];
+            
+            if (isAdmin) {
+                await sock.groupParticipantsUpdate(m.chat, [targetUser], 'remove');
+                sock.sendMessage(m.chat, {text:`Usuario @${user} expulsado del grupo por @${user}`, contextInfo:{remoteJid:m.chat,mentionedJid: [m.sender]}}, { quoted: m });
+            } else {
+                sock.sendMessage(m.chat, {text:'Solo los administradores pueden expulsar a miembros del grupo.'}, { quoted: m });
+            }
+        }catch (error) {
+            console.error('Error:', error);
+            sock.sendMessage(m.chat, {text:'Error al ejecutar el comando'}, { quoted: m });
+            
+        }
+        
+    },
 };
