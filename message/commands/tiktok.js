fcb9043b0cbb78b9fe4ffa8396333ba1ca04ac04
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 01:59:05 -0500

    update tiktok.js

--- message/commands/tiktok.js
+++ message/commands/tiktok.js
@@ -1,7 +1,5 @@
-// Importa las funciones necesarias
-const { fetchJson } = require('../../lib/utils')// Ajusta la ruta según la ubicación de tu función fetchJson
+const { fetchJson } = require('../../lib/utils')
 
-// Define el comando
 module.exports = {
     name: 'tiktok',
     description: 'Descarga videos e imágenes de TikTok',
@@ -10,13 +8,13 @@ module.exports = {
     async execute(sock, m, args) {
         try {
             if (!args[0]) {
-                v.reply('*tiktok <url>*');
+                await v.reply('*tiktok <url>*');
                 return;
             }
             const tiktokUrl = args[0];
             const response = await fetchJson(`https://star-apis.teamfx.repl.co/api/downloader/tiktok?url=${tiktokUrl}&apikey=StarAPI`);
             
-            sock.sendMessage(m.chat, {react: {text: '🕛',key: m.key,}})
+            await sock.sendMessage(m.chat, {react: {text: '🕛',key: m.key,}})
             
             function roundTime(time) {
                 return Math.round(time);
@@ -30,40 +28,40 @@ module.exports = {
                 const subCommand = args.shift().toLowerCase();
                 const result = response.result;
                 if (result.type === 'video') {
-                    sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
-                    sock.sendMessage(m.chat, {
+                    await sock.sendMessage(m.chat, {
                         video: { url: result.video.noWatermark },
                         mimetype: 'video/mp4',
                         caption: `ㅤ *⋯⋯ TIK TOK ⋯⋯*
  ∘ *Autor:* ${result.author.name}
  ∘ *Likes:* ${result.information.likeCount}
  ∘ *Comentarios:* ${result.information.commentCount}
+ ∘ *Duración:* ${result.video.durationFormatted}
  ∘ *Fecha:* ${result.information.created_at}
  ∘ *Titulo:* ${result.information.title}`
-                    }, {quoted:m});
+                    }, {quoted:m})
+                    await sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
                 } else if (result.type === 'images') {
-                    sock.sendMessage(m.chat, {
-                        audio: { url: result.music.url },
-                        mimetype: 'audio/mp4',
-                        ppt: true,
-                    }, { quoted: m });
                     for (const image of result.images) {
-                        sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
-                        sock.sendMessage(m.chat, {
+                        await sock.sendMessage(m.chat, {
                             image: { url: image.url.url, mimetype: 'image/jpeg' },
                             caption: `᳃ ¡Listo! - *🧃 ${formattedResponseTime} ms*`
                         }, {quoted:m});
                     }
-                    
+                    await sock.sendMessage(m.chat, {
+                        audio: { url: result.music.url },
+                        mimetype: 'audio/mp4',
+                        ppt: true,
+                    }, { quoted: m });
+                    await sock.sendMessage(m.chat, {react: {text: '✅',key: m.key,}})
                 }
-      } else {
-        console.log('Error al obtener información');
-        sock.sendMessage(m.chat, {react: {text: '❎',key: m.key,}})
-        v.reply(`No se pudo descargar`);
-      }
-    } catch (error) {
-      console.log('Error:', error);
-      v.reply('Error');
-    }
-  },
+            } else {
+                console.log('Error al obtener información');
+                await sock.sendMessage(m.chat, {react: {text: '❎',key: m.key,}})
+                await v.reply(`No se pudo descargar`);
+            }
+        } catch (e) {
+            console.log(e);
+            v.reply(`${e}`);
+        }
+    },
 };
