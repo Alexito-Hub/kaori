9cfdcca7fa72ff3f58b389f42753bb3e3bee8629
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 17:17:24 -0500

    update kick.js

--- message/commands/kick.js
+++ message/commands/kick.js
@@ -6,23 +6,25 @@ module.exports = {
     async execute(sock, m, args) {
         try {
             if (!m.isGroup) {
+                sock.sendMessage(m.chat, { text: 'Este comando solo se puede usar en grupos.' }, { quoted: m });
                 return;
             }
 
             const groupInfo = await sock.groupMetadata(m.chat);
-            
+
             const botNumber = sock.user.id.split(':')[0];
-            const isAdminBot = groupInfo && sock.getGroupAdmins(groupInfo.participants).includes(botNumber);
+            const isAdmin = groupInfo && sock.getGroupAdmins(groupInfo.participants).includes(botNumber);
             
-            if (!isAdminBot) {
+            if (!isAdmin) {
                 sock.sendMessage(m.chat, { text: 'El bot necesita ser administrador del grupo para usar este comando.' }, { quoted: m });
                 return;
             }
+
+            const senderNumber = m.sender.split('@')[0];
+            const isAdminUser = groupInfo && groupInfo.participants.some(p => p.id == senderNumber && ['admin', 'superadmin'].includes(p.admin));
             
-            const isAdmin = groupInfo && groupInfo.participants.some(p => p.id == m.sender && ['admin', 'superadmin'].includes(p.admin));
-            
-            if (!isAdmin) {
-                sock.sendMessage(m.chat, { text: 'Solo administradores.' }, { quoted: m });
+            if (!isAdminUser) {
+                sock.sendMessage(m.chat, { text: 'Solo los administradores pueden expulsar a miembros del grupo.' }, { quoted: m });
                 return;
             }
 
@@ -32,19 +34,19 @@ module.exports = {
             } else if (m.quoted) {
                 targetUser = m.quoted.sender;
             } else {
-                sock.sendMessage(m.chat, { text: '*kick <@usuario>*' }, { quoted: m });
+                sock.sendMessage(m.chat, { text: 'Usa el formato *kick <@usuario>* o responde al mensaje del usuario que deseas expulsar.' }, { quoted: m });
                 return;
             }
 
             const userObj = groupInfo.participants.find(p => p.id === targetUser);
             
             if (!userObj) {
-                sock.sendMessage(m.chat, { text: '¿?' }, { quoted: m });
+                sock.sendMessage(m.chat, { text: 'El usuario especificado no está en el grupo.' }, { quoted: m });
                 return;
             }
 
             if (['admin', 'superadmin'].includes(userObj.admin)) {
-                sock.sendMessage(m.chat, { text: 'No puedes eliminar a un administrador.' }, { quoted: m });
+                sock.sendMessage(m.chat, { text: 'No puedes expulsar a un administrador del grupo.' }, { quoted: m });
                 return;
             }
 
@@ -52,16 +54,16 @@ module.exports = {
             sock.sendMessage(m.chat, {
                 contextInfo: {
                     remoteJid: m.chat,
-                    mentionedJid: [m.sender, targetUser],
+                    mentionedJid: [targetUser],
                 },
                 video: { url: 'https://telegra.ph/file/25ec490a6f4dd4b423110.mp4' },
                 gifPlayback: true,
-                caption: `Usuario @${targetUser.split('@')[0]} expulsado del grupo por @${m.sender.split('@')[0]}`,
+                caption: `Usuario @${targetUser.split('@')[0]} expulsado del grupo por @${senderNumber}`,
             });
 
         } catch (error) {
-            console.log('Error:', error);
-            sock.sendMessage(m.chat, { text: `${error}` }, { quoted: m });
+            console.error('Error:', error);
+            sock.sendMessage(m.chat, { text: 'Error al ejecutar el comando' }, { quoted: m });
         }
     },
 };
