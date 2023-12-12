01d1d914afc8c400d524e17c9321e3f733bb661d
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-12 15:13:46 -0500

    update support.js

--- message/commands/support.js
+++ message/commands/support.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-require('../../config'); // Asegúrate de tener la referencia adecuada al archivo de configuración
+require('../../config');
 
 const ticketsFile = path.join(__dirname, 'tickets.json');
 let tickets = [];
@@ -15,7 +15,7 @@ module.exports = {
     
     async execute(sock, m, args) {
         try {
-            const isOwner = owner + '@s.whatsapp.net'
+            const isOwner = owner + '@s.whatsapp.net';
             const user = m.sender.split('@')[0];
             const supportMessage = args.join(' ');
 
@@ -30,10 +30,29 @@ module.exports = {
 
             fs.writeFileSync(ticketsFile, JSON.stringify(tickets, null, 2));
 
-            // Enviar mensaje de ticket al propietario del bot
-            await sock.sendMessage(isOwner, { text: `Nuevo ticket de soporte de ${user}:\n\n${supportMessage}` });
+            // Enviar mensaje de confirmación con emoji 🎫
+            const confirmationMessage = `Está a punto de crear un ticket\nRazón: ${supportMessage}\n\nPara continuar reaccione al mensaje con "🎫" o responde al mensaje con ticket`;
+            const confirmationResponse = await sock.sendMessage(m.chat, { text: confirmationMessage });
 
-            await sock.sendMessage(m.chat, { text: 'Tu mensaje de soporte ha sido enviado. El propietario revisará tu solicitud.' });
+            // Agregar reacción al mensaje de confirmación
+            await sock.react(confirmationResponse.key, '🎫');
+
+            // Esperar a que los usuarios reaccionen
+            const reactionTimeout = 60000; // 60 segundos
+            const reactions = await sock.getReactions(confirmationResponse.key, reactionTimeout);
+
+            // Verificar si alguien reaccionó con 🎫
+            const ticketReactions = reactions.filter(reaction => reaction.emoji === '🎫');
+
+            if (ticketReactions.length > 0) {
+                // Enviar mensaje al propietario
+                await sock.sendMessage(isOwner, { text: `Nuevo ticket de soporte de ${user}:\n\n${supportMessage}` });
+                await sock.sendMessage(m.chat, { text: 'Tu mensaje de soporte ha sido enviado. El propietario revisará tu solicitud.' });
+            } else {
+                // Nadie reaccionó, informar al usuario
+                await sock.sendMessage(m.chat, { text: 'Nadie reaccionó al mensaje de confirmación con 🎫. El ticket no ha sido procesado.' });
+            }
+
         } catch (error) {
             console.error('Error:', error);
             sock.sendMessage(m.chat, { text: 'Error al procesar la solicitud de soporte.' });
