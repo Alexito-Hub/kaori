92c6322c1988187c34761c08413742ef1755e343
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-12 15:37:07 -0500

    update support.js

--- message/commands/support.js
+++ message/commands/support.js
@@ -18,7 +18,7 @@ module.exports = {
             const user = m.sender.split('@')[0];
             const supportMessage = args.join(' ');
 
-            // Crear mensaje de confirmación y esperar reacciones
+            // Crear mensaje de confirmación
             const confirmationResponse = await sock.sendMessage(m.chat, {
                 text: `Está a punto de crear un ticket\n\nRazón: ${supportMessage}\n\nPara continuar reaccione al mensaje con "🎫" o responde al mensaje con "ticket"`,
             });
@@ -26,36 +26,32 @@ module.exports = {
             // Obtener el ID del mensaje de confirmación
             const confirmationMsgID = confirmationResponse.key.id;
 
-            // Esperar a que los usuarios reaccionen al mensaje de confirmación
-            const reactionTimeout = 60000; // 60 segundos
-            const reactions = await sock.messageReactions(confirmationMsgID, reactionTimeout);
+            // Esperar eventos de nuevos mensajes
+            sock.on('message-new', async (message) => {
+                // Verificar que sea una reacción y que sea al mensaje de confirmación
+                if (message.messageStubType === 14 && message.messageStubParameters && message.messageStubParameters.msgId === confirmationMsgID) {
+                    const userID = message.sender.split('@')[0];
 
-            // Filtrar las reacciones que tienen el emoji 🎫
-            const ticketReactions = reactions.filter(reaction => reaction.emoji === '🎫');
+                    const ticket = {
+                        user,
+                        userID,
+                        message: supportMessage,
+                        date: new Date().toLocaleString(),
+                        closed: false,
+                    };
 
-            // Procesar las reacciones y crear tickets
-            for (const reaction of ticketReactions) {
-                const userID = reaction.jid;
+                    tickets.push(ticket);
 
-                const ticket = {
-                    user,
-                    userID,
-                    message: supportMessage,
-                    date: new Date().toLocaleString(),
-                    closed: false,
-                };
+                    // Enviar mensaje de ticket al propietario del bot
+                    await sock.sendMessage(owner + '@s.whatsapp.net', { text: `Nuevo ticket de soporte de ${user}:\n\n${supportMessage}` });
 
-                tickets.push(ticket);
+                    // Enviar mensaje de confirmación al usuario que reaccionó
+                    await sock.sendMessage(userID, { text: 'Tu mensaje de soporte ha sido enviado. El propietario revisará tu solicitud.' });
 
-                // Enviar mensaje de ticket al propietario del bot
-                await sock.sendMessage(owner + '@s.whatsapp.net', { text: `Nuevo ticket de soporte de ${user}:\n\n${supportMessage}` });
-
-                // Enviar mensaje de confirmación al usuario que reaccionó
-                await sock.sendMessage(userID, { text: 'Tu mensaje de soporte ha sido enviado. El propietario revisará tu solicitud.' });
-            }
-
-            // Guardar los tickets en el archivo
-            fs.writeFileSync(ticketsFile, JSON.stringify(tickets, null, 2));
+                    // Guardar los tickets en el archivo
+                    fs.writeFileSync(ticketsFile, JSON.stringify(tickets, null, 2));
+                }
+            });
         } catch (error) {
             console.error('Error:', error);
             sock.sendMessage(m.chat, { text: 'Error al procesar la solicitud de soporte.' });
