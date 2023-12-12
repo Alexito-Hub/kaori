85107a88f2d534fb7f4281ae24f8f9781b246bb9
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-11 23:42:01 -0500

    update youtube.js

--- message/commands/youtube.js
+++ message/commands/youtube.js
@@ -1,103 +1,51 @@
 const ytdl = require('ytdl-core');
-const { createReadStream } = require('fs');
-
-const sizeLimit = 100 * 1024 * 1024;
 
 module.exports = {
     name: 'youtube',
-    description: 'Descarga y envía un video de YouTube en formato 720p',
-    aliases: ['yt', 'download'],
-
+    description: 'Descarga video o audio de YouTube',
+    
     async execute(sock, m, args) {
         try {
-            if (args.length !== 1) {
-                v.reply('*youtube <url>*');
+            if (!args[0]) {
+                sock.sendMessage(m.chat, { text: '*youtube <URL>*' }, { quoted: m });
                 return;
             }
 
             const youtubeUrl = args[0];
+            const isAudio = args.includes('--audio') || args.includes('-a');
 
-            if (await validateUrl(youtubeUrl)) {
-                sock.sendMessage(m.chat, { react:{ text: '🕛', key: m.key }});
+            const info = await ytdl.getInfo(youtubeUrl);
+            const format = isAudio ? ytdl.chooseFormat(info.formats, { quality: 'highestaudio' }) : ytdl.chooseFormat(info.formats, { quality: 'highest' });
 
-                const video = await downloadYoutubeVideo(youtubeUrl);
+            if (!format) {
+                sock.sendMessage(m.chat, { text: 'No se pudo obtener el formato del video o audio.' }, { quoted: m });
+                return;
+            }
 
-                if (video) {
-                    await sendVideo(sock, m, video);
-                } else {
-                    v.reply('El video supera el límite de 100 MB.');
-                }
+            const readableStream = ytdl.downloadFromInfo(info, { format });
+
+            // Puedes adaptar esto según tu necesidad, por ejemplo, guardar el archivo o enviarlo como mensaje
+            if (isAudio) {
+                sock.sendMessage(m.chat, {
+                    audio: {
+                        url: format.url,
+                        mimetype: format.mimeType,
+                        ptt: true
+                    }
+                }, { quoted: m });
             } else {
-                v.reply('URL de YouTube no válida.');
+                // Envía el video como un mensaje
+                sock.sendMessage(m.chat, {
+                    video: {
+                        url: format.url,
+                        mimetype: format.mimeType,
+                        filename: 'video.mp4'
+                    }
+                }, { quoted: m });
             }
         } catch (error) {
-            console.log(error);
-            v.reply('Error al procesar la solicitud.');
+            console.error('Error:', error);
+            sock.sendMessage(m.chat, { text: 'Error al ejecutar el comando' }, { quoted: m });
         }
     },
-};
-
-const validateUrl = async (url) => {
-    try {
-        const info = await ytdl.getInfo(url);
-        return info && info.videoDetails && info.videoDetails.lengthSeconds;
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
-};
-
-const downloadYoutubeVideo = async (url) => {
-    try {
-        const info = await ytdl.getInfo(url);
-
-        const format = ytdl.chooseFormat(info.formats, { quality: '136', filter: format => format.container === 'mp4' });
-
-        if (format && format.url) {
-            const stream = ytdl.downloadFromInfo(info, { format: format });
-
-            let buffer = Buffer.alloc(0);
-
-            return new Promise((resolve, reject) => {
-                stream.on('progress', (chunkLength, downloaded, total) => {
-                    if (buffer.length + chunkLength <= sizeLimit) {
-                        buffer = Buffer.concat([buffer, stream.read(chunkLength)]);
-                    } else {
-                        stream.destroy();
-                        reject(new Error('El video supera el límite de 100 MB.'));
-                    }
-                });
-
-                stream.on('end', () => {
-                    if (buffer.length <= sizeLimit) {
-                        resolve({ buffer: buffer, mimetype: 'video/mp4', caption: 'Video descargado de YouTube' });
-                    } else {
-                        reject(new Error('El video supera el límite de 100 MB.'));
-                    }
-                });
-
-                stream.on('error', reject);
-            });
-        } else {
-            throw new Error('Formato de video no válido.');
-        }
-    } catch (error) {
-        console.error(error);
-        throw new Error('Error al obtener información del video.');
-    }
-};
-
-const sendVideo = async (sock, m, video) => {
-    try {
-        const media = {
-            video: Buffer.from(video.buffer),
-            mimetype: video.mimetype,
-            caption: video.caption,
-        };
-
-        await sock.sendMessage(m.chat, media, { quoted: m});
-    } catch (error) {
-        console.error(error);
-        throw new Error('Error al enviar el video.');
-    }
 };
