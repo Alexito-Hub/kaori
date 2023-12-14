75a1e58333c08b3dce7130e79466e6557604403e
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-14 13:31:59 -0500

    update play.js

--- message/commands/play.js
+++ message/commands/play.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 
 module.exports = {
     name: 'play',
-    description: 'Buscar y reproducir el primer video relacionado con la búsqueda.',
-    aliases: [],
+    description: 'Buscar y mostrar información del primer video relacionado con la búsqueda.',
+    aliases: ['p'],
 
     async execute(sock, m, args) {
         try {
             if (!args[0]) {
-                return v.reply('Por favor, proporciona una búsqueda.');
+                return sock.sendMessage(m.chat, { text: 'Por favor, proporciona una búsqueda.' });
             }
 
             const searchQuery = args.join(' ');
@@ -17,20 +17,41 @@ module.exports = {
 
             if (searchResults && searchResults.items && searchResults.items.length > 0) {
                 const firstVideo = searchResults.items[0];
-                const videoUrl = `https://star-apis.teamfx.repl.co/api/downloader/ytplay?url=${encodeURIComponent(firstVideo.url)}&apikey=StarAPI`;
+                const videoInfo = `*Título:* ${firstVideo.title}\n*Duración:* ${firstVideo.duration || 'Desconocida'}\n*Visitas:* ${firstVideo.views || 'Desconocido'}\n*Enlace:* ${firstVideo.url}`;
 
-                await sock.sendMessage(m.chat, { 
-                    video: { url: videoUrl },
-                    mimetype: 'video/mp4',
-                    caption: `hecho`
-                    
-                }, { quoted: m });
+                await sock.sendMessage(m.chat, { text: `Información del Video:\n${videoInfo}`, quoted: m });
+
+                const confirmationMessage = '¿Quieres descargar el video como MP4 o MP3?\n\n1. MP4\n2. MP3';
+                await sock.sendMessage(m.chat, { text: confirmationMessage });
+
+                const response = await sock.receiveMessage(m.chat);
+                const choice = response.text.toLowerCase();
+
+                if (choice === '1' || choice === 'mp4') {
+                    await downloadAndSendVideo(sock, m, firstVideo.url, 'video/mp4');
+                } else if (choice === '2' || choice === 'mp3') {
+                    await downloadAndSendVideo(sock, m, firstVideo.url, 'audio/mp3');
+                } else {
+                    sock.sendMessage(m.chat, { text: 'Opción no válida. La descarga ha sido cancelada.' });
+                }
             } else {
-               v.reply('No se encontraron resultados para la búsqueda.');
+                sock.sendMessage(m.chat, { text: 'No se encontraron resultados para la búsqueda.' });
             }
         } catch (error) {
             console.error('Error en el comando play:', error);
-            v.reply('Se produjo un error al ejecutar el comando play.');
+            sock.sendMessage(m.chat, { text: 'Se produjo un error al ejecutar el comando play.' });
         }
     },
 };
+
+async function downloadAndSendVideo(sock, m, videoUrl, mimeType) {
+    try {
+        const downloadUrl = `https://star-apis.teamfx.repl.co/api/downloader/ytplay?url=${encodeURIComponent(videoUrl)}&apikey=StarAPI`;
+
+        await sock.sendMessage(m.chat, { text: 'Descargando el video...', quoted: m });
+        await sock.sendMessage(m.chat, { video: { url: downloadUrl }, mimetype: mimeType, quoted: m });
+    } catch (error) {
+        console.error('Error al descargar y enviar el video:', error);
+        sock.sendMessage(m.chat, { text: 'Se produjo un error al descargar y enviar el video.' });
+    }
+}
